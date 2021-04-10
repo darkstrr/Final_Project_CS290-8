97845f98ab0dde2a81ca27364dcd475b73b41f05
Akshay Patel <ec2-user@ip-172-31-55-144.ec2.internal>
2021-04-10 14:29:28 +0000

    Google Login is working. Username, Email, and profile picture is display on the UI.

--- src/components/Login.js
+++ src/components/Login.js
@@ -31,22 +31,22 @@ function Login() {
   
   //Refresh login Token 
   const refreshToken = (res) => {
-  // Timing to renew access token
-  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
-
-  const refreshToken = async () => {
-    const newAuthRes = await res.reloadAuthResponse();
-    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
-    console.log('newAuthRes:', newAuthRes);
+    // Timing to renew access token
+    let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
+  
+    const refreshToken = async () => {
+        const newAuthRes = await res.reloadAuthResponse();
+        refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
+        console.log('newAuthRes:', newAuthRes);
+        
+        console.log('new auth Token', newAuthRes.id_token);
     
-    console.log('new auth Token', newAuthRes.id_token);
-
-    // Setup the other timer after the first one
+        // Setup the other timer after the first one
+        setTimeout(refreshToken, refreshTiming);
+    };
+  
+    // Setup first refresh timer
     setTimeout(refreshToken, refreshTiming);
-  };
-
-  // Setup first refresh timer
-  setTimeout(refreshToken, refreshTiming);
 };
   
   const onFailure = (res) => {
