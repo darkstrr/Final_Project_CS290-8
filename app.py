ff8077103313c50a0088b6c3891bffb456d512e2
Marco Paparatto <ec2-user@ip-172-31-24-135.ec2.internal>
2021-04-16 17:30:19 +0000

    Cleaned up code, added more artists

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ from music_fetch import MusicFetch
 
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv()) # This is to load your API keys from .env
-
+load_dotenv(find_dotenv())  # This is to load your API keys from .env
 
 #Flask app name
 app = Flask(__name__, static_folder='./build/static')
@@ -25,24 +24,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
-
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app,
                     cors_allowed_origins="*",
                     json=json,
                     manage_session=False)
 
+
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
-    
+
+
 # When a client connects from this Socket connection, this function is run
 @socketio.on('connect')
 def on_connect():
     """function print message into console when connetion is ON"""
     print('User connected!')
+
 
 # When a client disconnects from this Socket connection, this function is run
 @socketio.on('disconnect')
@@ -50,12 +50,14 @@ def on_disconnect():
     """function print message into console when disconnected is ON"""
     print('User disconnected!')
 
+
 @socketio.on('start')
 def on_start():
     """function starts when the start game button is pressed"""
     tracks = MusicFetch()
     socketio.emit('tracks', tracks, broadcast=True)
-    
+
+
 # Note we need to add this line so we can import app in the python shell
 if __name__ == "__main__":
     # Note that we don't call app.run anymore. We call socketio.run with app arg
@@ -65,4 +67,3 @@ if __name__ == "__main__":
         host=os.getenv('IP', '0.0.0.0'),
         port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8080)),
     )
-
--- music_fetch.py
+++ music_fetch.py
@@ -3,40 +3,50 @@ from dotenv import load_dotenv, find_dotenv
 import os
 from random import randint, shuffle
 
+
 def MusicFetch():
-    artists = ['4tZwfgrHOc3mvqYlEYSvVi','4YRxDV8wJFPHPTeXepOstw','3xU8YsNNkmWSPewlB18NUz','5f4QpKfy7ptCHwTqspnSJI','6vWDO969PvNqNYHIOW5v0m','7jefIIksOi1EazgRTfW2Pk']
-    return_list=[]
+    artists = [
+        '4tZwfgrHOc3mvqYlEYSvVi', '4YRxDV8wJFPHPTeXepOstw',
+        '3xU8YsNNkmWSPewlB18NUz', '5f4QpKfy7ptCHwTqspnSJI',
+        '6vWDO969PvNqNYHIOW5v0m', '7jefIIksOi1EazgRTfW2Pk',
+        '0oSGxfWSnnOXhD2fKuz2Gy'
+    ]
+    return_list = []
     selected_tracks = []
     """function fetching music for the game"""
     load_dotenv(find_dotenv())
-    client = SpotifyAPI(os.getenv('client_id'),os.getenv('client_secret'))
+    client = SpotifyAPI(os.getenv('client_id'), os.getenv('client_secret'))
     client.get_access_token()
-    for y in range (0,5):
-        tracks = client.get_list_of_track_info(artists[randint(0,(len(artists) - 1))])
-        for x in range(0,4):
+    for y in range(0, 5):
+        tracks = client.get_list_of_track_info(artists[randint(
+            0, (len(artists) - 1))])
+        for x in range(0, 4):
             print(len(tracks))
-            track = randint(0,(len(tracks) - 1))
+            track = randint(0, (len(tracks) - 1))
             select = False
             while not select:
                 if tracks[track]['song_preview_url']:
                     selected_tracks.append(tracks[track])
                     select = True
                 tracks.pop(track)
-        return_list.append(
-            {
-                "questionText": selected_tracks[0]['song_preview_url'],
-                "answerOptions": [
-                        { "answerText": selected_tracks[0]["song_name"], "isCorrect": True }
-                    ]
-                    }
-            )
-        selected_tracks.pop(0)    
-        for x in range (0,3):
-            return_list[y]['answerOptions'].append({"answerText": selected_tracks[x]["song_name"], "isCorrect": False})
-        
-        shuffle(return_list[y]["answerOptions"])    
-        
-    print(return_list)    
+        return_list.append({
+            "questionText":
+            selected_tracks[0]['song_preview_url'],
+            "answerOptions": [{
+                "answerText": selected_tracks[0]["song_name"],
+                "isCorrect": True
+            }]
+        })
+        selected_tracks.pop(0)
+        for x in range(0, 3):
+            return_list[y]['answerOptions'].append({
+                "answerText":
+                selected_tracks[x]["song_name"],
+                "isCorrect":
+                False
+            })
+
+        shuffle(return_list[y]["answerOptions"])
+
+    print(return_list)
     return return_list
-        
-    
