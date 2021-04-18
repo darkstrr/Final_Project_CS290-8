11888597920bab89ea9a7ad9d431168f67ea3e0f
Shayed Ahmed <supershayed@gmail.com>
2021-04-18 19:32:55 +0000

    Added a test for user login

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,17 @@
 /*eslint-disable */
 import { render, screen, fireEvent } from '@testing-library/react';
-import  App from './App';
-import { mount } from 'enzyme';
+import "@testing-library/jest-dom";
+import App from './App';
+
+test("User is able to successfully login", () => {
+  render(<App />);
+
+  const googleLoginButton  = screen.getByText("Google Login"); // Get google login button by text
+  expect(googleLoginButton).toBeInTheDocument(); // check if button is on page
+  fireEvent.click(googleLoginButton); // click the login button
+  
+  // Get google logout button by text
+  const googleLogoutButton  = screen.getByText("Logout");
+  // Logout button present which means you are logged in
+  expect(googleLogoutButton).toBeInTheDocument();
+});
