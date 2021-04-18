7808c8a623f0d998d16cd27826259b89ee086d88
Shayed Ahmed <supershayed@gmail.com>
2021-04-18 19:36:59 +0000

    Added second test to check if user can start game and view questions

--- src/App.test.js
+++ src/App.test.js
@@ -15,3 +15,14 @@ test("User is able to successfully login", () => {
   // Logout button present which means you are logged in
   expect(googleLogoutButton).toBeInTheDocument();
 });
+
+test("Checks to see if music game can be started", () => {
+  render(<App />);
+
+  const googleLoginButton  = screen.getByText("Google Login"); // Get google login button by text
+  expect(googleLoginButton).toBeInTheDocument(); // check if button is on page
+  fireEvent.click(googleLoginButton); // click the login button
+  
+  const startGameButton = screen.getByTestId("start_game");
+  expect(startGameButton).toBeInTheDocument();
+});
