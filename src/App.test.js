6e0fe92f7f4a49277a680b55e64ab8b35343cd6a
Shayed Ahmed <supershayed@gmail.com>
2021-04-18 19:44:34 +0000

    Added documentation on how second unit test works for jesting

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ test("User is able to successfully login", () => {
   render(<App />);
 
   const googleLoginButton  = screen.getByText("Google Login"); // Get google login button by text
-  expect(googleLoginButton).toBeInTheDocument(); // check if button is on page
+  expect(googleLoginButton).toBeInTheDocument(); // check if login button is on page
   fireEvent.click(googleLoginButton); // click the login button
   
   // Get google logout button by text
@@ -20,13 +20,17 @@ test("Checks to see if music game can be started", () => {
   render(<App />);
 
   const googleLoginButton  = screen.getByText("Google Login"); // Get google login button by text
-  expect(googleLoginButton).toBeInTheDocument(); // check if button is on page
+  expect(googleLoginButton).toBeInTheDocument(); // check if login button is on page
   fireEvent.click(googleLoginButton); // click the login button
   
+  // Retrieves start button
   const startGameButton = screen.getByTestId("start_game");
+  // check if start button is on page
   expect(startGameButton).toBeInTheDocument();
+  // click the start button
   fireEvent.click(startGameButton);
   
   const questionText = screen.getByText("Question");
+  // checks to see if question text is on screen after clicking start game button
   expect(questionText).toBeInTheDocument();
 });
