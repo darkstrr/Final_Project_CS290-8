8562bfd94e8bca3e07713c3e12444df8c9218737
Shayed Ahmed <supershayed@gmail.com>
2021-04-19 17:47:53 +0000

    Updated first test with newer logic

--- src/App.test.js
+++ src/App.test.js
@@ -2,35 +2,21 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import "@testing-library/jest-dom";
 import App from './App';
+import { mount , component , wrapper, find} from 'enzyme';
+import  Logout  from './components/Logout.js';
+import  Game  from './components/Game.js';
 
-test("User is able to successfully login", () => {
+
+test("User is able to successfully login and logout", () => {
   render(<App />);
-         
+
+  //Login test
   const googleLoginButton  = screen.getByText("Google Login"); // Get google login button by text
   expect(googleLoginButton).toBeInTheDocument(); // check if login button is on page
   fireEvent.click(googleLoginButton); // click the login button
-  
-  // Get google logout button by text
-  const googleLogoutButton  = screen.getByText("Logout");
-  // Logout button present which means you are logged in
-  expect(googleLogoutButton).toBeInTheDocument();
-});
 
-test("Checks to see if music game can be started", () => {
-  render(<App />);
+  //Logout test
+  const wrapper = mount(<Logout />);
+  expect(wrapper.text()).toMatch("Logout");
 
-  const googleLoginButton  = screen.getByText("Google Login"); // Get google login button by text
-  expect(googleLoginButton).toBeInTheDocument(); // check if login button is on page
-  fireEvent.click(googleLoginButton); // click the login button
-  
-  // Retrieves start button
-  const startGameButton = screen.getByTestId("start_game");
-  // check if start button is on page
-  expect(startGameButton).toBeInTheDocument();
-  // click the start button
-  fireEvent.click(startGameButton);
-  
-  const questionText = screen.getByText("Question");
-  // checks to see if question text is on screen after clicking start game button
-  expect(questionText).toBeInTheDocument();
 });
