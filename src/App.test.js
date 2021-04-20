22e2a1c4edf217db2a42276b734b657517728772
Shayed Ahmed <supershayed@gmail.com>
2021-04-20 19:36:51 +0000

    Second jest test added for login button disappearing after logging in

--- src/App.test.js
+++ src/App.test.js
@@ -20,3 +20,17 @@ test("User is able to successfully view logout button after logging in", () => {
   // checks if logout button exists
   expect(wrapper.text()).toMatch("Logout");
 });
+
+test("Login button dissapears after user is done logging in", () => {
+  render(<App />);
+  
+  const googleLoginButton  = screen.getByText("Google Login"); // Get google login button by text
+  expect(googleLoginButton).toBeInTheDocument(); // check if login button is on page
+  fireEvent.click(googleLoginButton); // click the login button
+  
+  // Gets Google Login text on button
+  const startElement= screen.queryByText('Google Login:');
+  // checks if button does not exist after logging in
+  expect(startElement).not.toBeInTheDocument();
+
+});
